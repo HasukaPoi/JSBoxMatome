065eea5b4c84451172d8a70839b70c248e1dea45
HasukaPc <hasukapyon@163.com>
2018-06-08 23:36:49 +0800

    Add quailty selection

--- yandereGetter2.js
+++ yandereGetter2.js
@@ -7,7 +7,7 @@ $app.strings = {
     "PREV": "Prev",
     "NEXT": "Next",
     "INVALID_PAGE": "Invalid Page",
-    "DL": "DL SQ",
+    "DL": "DL",
     "DL_H": "DL HQ",
     "SAVED": "Saving succeed",
     "SHARE": "Ready to share?",
@@ -22,7 +22,7 @@ $app.strings = {
     "PREV": "←",
     "NEXT": "→",
     "INVALID_PAGE": "页码无效",
-    "DL": "保存小图",
+    "DL": "保存",
     "DL_H": "保存大图 ",
     "SAVED": "已保存到相册",
     "SHARE": "是否进行分享？",
@@ -49,12 +49,10 @@ $ui.render({
     },
     events: {
       returned: function (sender) {
-        $console.info("returned")
-        currentPage = 1
-        search()
+        newSearch()
       }
     }
-  }, {
+  }/* , {
     type: "button",
     props: {
       title: $l10n("SEARCH"),
@@ -67,27 +65,28 @@ $ui.render({
     },
     events: {
       tapped: function (sender) {
-        currentPage = 1
-        $cache.set("keyw", $("keyword").text)
-        search()
+        newSearch()
       }
     }
-  }, {
+  } */, {
     type: "button",
     props: {
       title: $l10n("PREV"),
       id: "prev"
     },
     layout: function (make, view) {
-      make.left.equalTo($("search").right).offset(15)
+      make.left.equalTo($("keyword").right).offset(15)
       make.centerY.equalTo($("keyword"))
       make.width.equalTo(32)
     },
     events: {
       tapped: function (sender) {
-        currentPage--
         if (currentPage > 0) {
-          search()
+          if (currentPage > 1) {
+            currentPage--
+            search()
+          } else { $ui.error("已经是第一页了") }
+
         } else {
           $ui.error($l10n("INVALID_PAGE"))
         }
@@ -140,24 +139,24 @@ $ui.render({
     events: {
       tapped: function (sender) {
         $ui.menu({
-          items: ["设置列数","设置每页图片数量"],
-          handler:function(title,idx){
-            if(idx==0){
+          items: ["设置列数", "设置每页图片数量"],
+          handler: function (title, idx) {
+            if (idx == 0) {
               $ui.menu({
-                items: [1, 2, 3, 4, 5].map(function(item) { return item}),
-                handler: function(title, idx) {
+                items: [1, 2, 3, 4, 5].map(function (item) { return item }),
+                handler: function (title, idx) {
                   var count = idx + 1
-                  $cache.set("columns",count)
+                  $cache.set("columns", count)
                   $ui.toast("该设置重启扩展后生效")
                 }
               })
-            } else if (idx==1){
+            } else if (idx == 1) {
               $ui.menu({
-                items: [20,30,40,50],
-                handler: function(title, idx) {
+                items: [20, 30, 40, 50],
+                handler: function (title, idx) {
                   var count = title
                   $console.info(count)
-                  $cache.set("limit",count)
+                  $cache.set("limit", count)
                   $ui.toast("该设置下次搜索生效")
                 }
               })
@@ -205,7 +204,33 @@ $ui.render({
     events: {
       didSelect: function (tableView, indexPath) {
         var post = tableView.object(indexPath)
-        preview(post)
+        //preview(post)
+        var message = {
+          title: "请选择需要的图片质量",
+          message: "若JPEG显示为0kB，则与origin为同文件",
+          actions: [{
+            title: "Sample " + makeSizeText(post.sample_width, post.sample_height, post.sample_size),
+            handler: function () {
+              preview(post, "sample")
+            }
+          }, {
+            title: "JPEG " + makeSizeText(post.jpeg_width, post.jpeg_height, post.jpeg_size),
+            handler: function () {
+              preview(post, "jpeg")
+            }
+          }, {
+            title: "Origin "+makeSizeText(0,0,post.file_size),
+            handler: function () {
+              preview(post, "origin")
+            }
+          },
+          {
+            title: "取消",
+            style: "Cancel"
+          }
+          ]
+        }
+        $ui.action(message)
       },
       pulled: function (sender) {
         search()
@@ -213,6 +238,20 @@ $ui.render({
     }
   }]
 })
+
+function makeSizeText(width, height, size) {
+  var unit = "kB"
+  size /= 1024
+  if (size > 1000) {
+    size /= 1024
+    size=size.toFixed(2)
+    unit = "MB"
+
+  } else size=size.toFixed(2)
+  if (width > 0) return "(" + width + "*" + height + ", " + size + unit + ")"
+  else return "(" + size + unit + ")"
+
+}
 
 function share(data) {
   var message = {
@@ -250,25 +289,36 @@ function render2(posts) {
       tags: { text: post.jpeg_width + "*" + post.jpeg_height },
       sample: post.sample_url,
       jpeg: post.jpeg_url,
+      origin: post.file_url,
       id: post.id,
+      file_size: post.file_size,
       jpeg_width: post.jpeg_width,
       jpeg_height: post.jpeg_height,
+      jpeg_size: post.jpeg_file_size,
       sample_width: post.sample_width,
-      sample_height: post.sample_height
+      sample_height: post.sample_height,
+      sample_size: post.sample_file_size
     })
   }
   $("matrix").data = data
   $("matrix").endRefreshing()
 }
 
+function newSearch() {
+  currentPage = 1
+  $cache.set("keyw", $("keyword").text.replace(/ /, "+"))
+  search()
+}
+
 function search() {
   $("keyword").blur()
   $("pageIn").text = currentPage
   if (currentPage == 0) return;
-  var keyword = $("keyword").text
-  $ui.loading("Downloading")
+  //var keyword = $("keyword").text
+  var keyword = $cache.get("keyw") ? $cache.get("keyw") : ""
+  $ui.loading(true)
   $http.get({
-    url: "https://yande.re/post.json?api_version=2&limit="+($cache.get("limit") ? $cache.get("limit") : 20)+"&tags=" + keyword + "&page=" + currentPage,
+    url: "https://yande.re/post.json?api_version=2&limit=" + ($cache.get("limit") ? $cache.get("limit") : 20) + "&tags=" + keyword + "&page=" + currentPage,
     handler: function (resp) {
       $ui.loading(false)
       render2(resp.data.posts)
@@ -280,87 +330,58 @@ function search() {
   })
 }
 
-function preview(post) {
-  // get preview
-  if ($cache.get(post.id)) {
-    showpreview(post)
+
+
+
+function preview(post, q) {
+  q = arguments[1] ? arguments[1] : "sample"
+  if ($cache.get(post.id + q)) {
+    showpreview(post, q)
   } else {
-    $ui.loading("Downloading")
+    $ui.toast("下载" + q + "中")
+    $ui.loading(true)
     $http.download({
-      url: post.sample,
+      url: post[q],
       handler: function (resp) {
         $ui.loading(false)
-        $cache.set(post.id, resp.data)
-        showpreview(post)
+        $cache.set(post.id + q, resp.data)
+        showpreview(post, q)
       }
     })
   }
 }
 
-function showpreview(post) {
-  $console.info(post.jpeg)
+function showpreview(post, q) {
+  q = arguments[1] ? arguments[1] : "sample";
   $ui.push({
     props: {
-      title: $l10n("PREVIEW") + ": " + post.id
+      title: $l10n("PREVIEW") + ": #" + post.id
     },
     views: [{
       type: "image",
       props: {
-        // src: post.sample,
-        data: $cache.get(post.id),
+        data: $cache.get(post.id + q),
         id: "current",
         contentMode: $contentMode.scaleAspectFit
       },
       layout: $layout.fill
-    },
-    {
+    }, {
       type: "button",
       props: {
-        title: $l10n("DL"),
+        title: $l10n("DL") + q,
         id: "dl"
       },
       layout: function (make, view) {
         make.left.bottom.inset(10)
         make.height.equalTo(32)
+
       },
       events: {
         tapped: function (sender) {
           $photo.save({
-            data: $cache.get(post.id),
+            data: $cache.get(post.id + q),
             handler: function (success) {
-              share($cache.get(post.id))
-            }
-          })
-        }
-      }
-    },
-    {
-      type: "button",
-      props: {
-        title: $l10n("DL_H") + "(" + post.jpeg_width + "*" + post.jpeg_height + ")",
-        id: "dl_h"
-      },
-      layout: function (make, view) {
-        make.left.equalTo($("dl").right).offset(10)
-        make.centerY.equalTo($("dl"))
-      },
-      events: {
-        tapped: function (sender) {
-          $ui.loading("Downloading")
-          $http.download({
-            url: post.jpeg,
-            progress: function (bytesWritten, totalBytes) {
-              var percentage = bytesWritten * 1.0 / totalBytes
-              $ui.progress(percentage)
-            },
-            handler: function (resp) {
-              $ui.loading(false)
-              $photo.save({
-                data: resp.data,
-                handler: function (success) {
-                  share(resp.data)
-                }
-              })
+              share($cache.get(post.id + q))
             }
           })
         }
@@ -368,6 +389,77 @@ function showpreview(post) {
     }]
   })
 }
+
+// function showpreview(post) {
+//   $console.info(post.jpeg)
+//   $ui.push({
+//     props: {
+//       title: $l10n("PREVIEW") + ": #" + post.id
+//     },
+//     views: [{
+//       type: "image",
+//       props: {
+//         // src: post.sample,
+//         data: $cache.get(post.id),
+//         id: "current",
+//         contentMode: $contentMode.scaleAspectFit
+//       },
+//       layout: $layout.fill
+//     },{
+//       type: "button",
+//       props: {
+//         title: $l10n("DL_H") + "(" + post.jpeg_width + "*" + post.jpeg_height + ")",
+//         id: "dl_h"
+//       },
+//       layout: function (make, view) {
+//         make.left.bottom.inset(10)
+//         make.height.equalTo(32)
+//       },
+//       events: {
+//         tapped: function (sender) {
+//           $ui.toast("下载大图中")
+//           $ui.loading(true)
+//           $http.download({
+//             url: post.jpeg,
+//             progress: function (bytesWritten, totalBytes) {
+//               var percentage = bytesWritten * 1.0 / totalBytes
+//               $ui.progress(percentage)
+//             },
+//             handler: function (resp) {
+//               $ui.loading(false)
+//               $photo.save({
+//                 data: resp.data,
+//                 handler: function (success) {
+//                   share(resp.data)
+//                 }
+//               })
+//             }
+//           })
+//         }
+//       }
+//     },{
+//       type: "button",
+//       props: {
+//         title: $l10n("DL"),
+//         id: "dl"
+//       },
+//       layout: function (make, view) {
+//         make.bottom.equalTo($("dl_h").top).offset(-10)
+//         make.left.equalTo($("dl_h"))
+//       },
+//       events: {
+//         tapped: function (sender) {
+//           $photo.save({
+//             data: $cache.get(post.id),
+//             handler: function (success) {
+//               share($cache.get(post.id))
+//             }
+//           })
+//         }
+//       }
+//     }]
+//   })
+// }
 
 var currentPage = 0;
 if ($cache.get("keyw")) {
