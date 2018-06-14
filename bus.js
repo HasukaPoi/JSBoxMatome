862decbde2c0a0d3a7ed735247ab1bafee86b88f
HasukaPc <hasukapyon@163.com>
2018-06-14 10:18:04 +0800

    ???

--- bus.js
+++ bus.js
@@ -27,17 +27,6 @@ var buses = [
     }
 ]
 
-// $ui.menu({
-//     items: [528, 529, "B1", "B10", "B12", "B13"],
-//     handler: function (title, idx) {
-//         $safari.open({
-//             url: "http://wap.czgj.cn:8081/MyBus/LineDetailQuery?lineId="
-//                 + title.replace(/B/, "8")
-//                 + "&direction=1"
-//         })
-//     }
-// })
-
 $ui.menu({
     items: buses.map(function (item) { return item.title }),
     handler: function (title, idx) {
