3b5d7967039df2b0b1eb2f59e9987bb1776026a2
Hasuka <hasukapyon@163.com>
2019-03-08 21:14:47 +0800

    edited

--- 日拍盯梢.js
+++ 日拍盯梢.js
@@ -4,7 +4,7 @@ var engines = [{
 }, {
   name: "駿河屋",
   pattern: "https://www.suruga-ya.jp/search?category=&search_word=[KEY]"
-}, ]
+},]
 
 var keywords = [
   "ゆずソフト",
@@ -29,30 +29,30 @@ if ($context.text) {
     }
   })
 } else {
-main()
-/*   $ui.menu({
-    items: ["【剪贴板】" + $clipboard.text, "【输入内容】"].concat(keywords),
-    handler: function (title, idx) {
-      switch (idx) {
-        case 0:
-          showTypes($clipboard.text);
-          break;
-        case 1:
-          $input.text({
-            handler: function (text) {
-              showTypes(text)
-            }
-          });
-          break;
-        default:
-          showTypes(title)
-
+  main()
+  /*   $ui.menu({
+      items: ["【剪贴板】" + $clipboard.text, "【输入内容】"].concat(keywords),
+      handler: function (title, idx) {
+        switch (idx) {
+          case 0:
+            showTypes($clipboard.text);
+            break;
+          case 1:
+            $input.text({
+              handler: function (text) {
+                showTypes(text)
+              }
+            });
+            break;
+          default:
+            showTypes(title)
+  
+        }
       }
-    }
-  }) */
+    }) */
 }
 
-function main(){
+function main() {
   let message = {
     //title: "请选择次级关键字",
     message: "请选择或输入关键字",
@@ -60,9 +60,9 @@ function main(){
       title: item,
       handler: () => showTypes(item)
     })).concat([{
-      title:"[剪贴板]"+$clipboard.text,
-      handler:() => showTypes($clipboard.text)
-    },{
+      title: "[剪贴板]" + $clipboard.text,
+      handler: () => showTypes($clipboard.text)
+    }, {
       title: "[输入关键字]",
       style: "cancel",
       handler: () => {
@@ -74,7 +74,7 @@ function main(){
       }
     }]),
   }
-    $ui.action(message)
+  $ui.action(message)
 
 }
 
@@ -85,7 +85,11 @@ function showTypes(key) {
     actions: types.map((item) => ({
       title: item,
       handler: () => showEngines(key + " " + item)
-    }))
+    })).concat([{
+      title: "无次级关键字",
+      style: "cancel",
+      handler: () => showEngines(key)
+    }])
   }
   if (key.indexOf(" ") === -1 || key.indexOf("+") === 0) {
     $ui.action(message)
@@ -104,18 +108,14 @@ function showEngines(text) {
         handler: () => {
           $thread.main({
             delay: 0.4,
-            handler: () => {
-              search(item.pattern, text)
-            }
+            handler: () => search(item.pattern, text)
           })
         }
       })
     ).concat([{
       title: "别手滑",
       style: "cancel",
-      handler: () => {
-        search(engines[0].pattern, text)
-      }
+      handler: () => search(engines[0].pattern, text)
     }])
   }
   $ui.action(message)
